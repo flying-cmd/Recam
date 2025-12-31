94442aa3d9b5ca35e5e41c8988d39513c3101151
feixiang <913737079@qq.com>
2026-01-01 01:30:39 +1100

    fix(RECAM-12): rename Avatar to AvatatUrl

--- Remp.Solution/Remp.Service/DTOs/AgentDto.cs
+++ Remp.Solution/Remp.Service/DTOs/AgentDto.cs
@@ -5,6 +5,6 @@ public class AgentDto
     public string Id { get; set; } = null!;
     public string AgentFirstName { get; set; } = null!;
     public string AgentLastName { get; set; } = null!;
-    public string Avatar { get; set; } = null!;
+    public string AvatarUrl { get; set; } = null!;
     public string CompanyName { get; set; } = null!;
 }
