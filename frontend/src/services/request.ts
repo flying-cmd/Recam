4baa6f19ddd8d63208e3dcb92262175d323e3892
feixiang <913737079@qq.com>
2026-03-06 17:51:39 +1100

    fix api request path

--- frontend/src/services/request.ts
+++ frontend/src/services/request.ts
@@ -3,7 +3,7 @@ import { unAuthorizedApiList } from "./unAuthorizedApiList";
 import type { IApiError, IExceptionResponse } from "../types/IApiResponse";
 
 export const service: AxiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
   timeout: 15000
 });
 
